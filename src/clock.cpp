20b29e285a8734f29823ae49cec29670f76ec54a
kocariz <kattalin.ocariz-monedero@epitech.eu>
2021-03-07 18:32:45 +0100

    added clock dump

--- src/clock.cpp
+++ src/clock.cpp
@@ -33,6 +33,15 @@ void nts::Clock::setLink(std::size_t pin, nts::IComponent *other,
 
 void nts::Clock::dump() const
 {
+    std::cout << "INPUT:" << std::endl;
+    std::cout << "\tname -> " << _name << std::endl;
+    std::cout << "\tstate of the input -> ";
+    if (getValue() == nts::UNDEFINED)
+        std::cout << "Undefined" << std::endl << std::endl;
+    else if (getValue() == nts::TRUE)
+        std::cout << "True" << std::endl << std::endl;
+    else if (getValue() == nts::FALSE)
+        std::cout << "False" << std::endl << std::endl;
 }
 
 void nts::Clock::setConnections(int pin1, int pin2, int outputPin)
