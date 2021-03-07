f6a508db2dc367c5bd675638cf64b76d073dfd68
Arnau Espejo <arnaueh@gmail.com>
2021-03-07 22:00:10 +0100

    true

--- include/components/true.hpp
+++ include/components/true.hpp
@@ -11,14 +11,15 @@
 
 namespace nts {
     class True : public IComponent {
-        True();
-        ~True();
-        void simulate(std::size_t tick) override;
-        Tristate compute(std::size_t pin) override;
-        void setLink(std::size_t pin, IComponent *other, std::size_t otherPin
-        ) override;
-        void dump() override;
-        void setConnections(int pin1, int pin2, int outputPin) override;
+        public:
+            True();
+            ~True();
+            void simulate(std::size_t tick) override;
+            Tristate compute(std::size_t pin) override;
+            void setLink(std::size_t pin, IComponent *other, std::size_t otherPin
+            ) override;
+            void dump() override;
+            void setConnections(int pin1, int pin2, int outputPin) override;
     };
 }
 #endif //NANOTEKSPICE_TRUE_HPP
